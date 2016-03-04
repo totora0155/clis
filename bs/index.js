104599fb17aeb84d66678f3f0185bd00ed19e0f4
nju33 <sski0155@gmail.com>
2016-03-04 13:29:41 +0900

    Add `bs`

---
+++ bs/index.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const assign = require('object-assign');
+const bs = require('browser-sync').create();
+const config = require('./bs-config');
+const cwd = process.cwd();
+const opts = require('minimist')(process.argv.slice(2));
+
+if (opts.e || opts.edit) {
+  // TODO: editor
+  process.exit(0);
+}
+
+if (opts._[0]) {
+  if (opts.p || opts.proxy) {
+    const proxy = opts._[0];
+    config.proxy = proxy;
+  } else {
+    const baseDir = opts._[0];
+    if (typeof config.server !== 'object') {
+      config.server = {};
+    }
+    config.server.baseDir = baseDir;
+  }
+} else {
+  if (typeof config.server !== 'object') {
+    config.server = {};
+  }
+  config.server.baseDir = cwd;
+}
+
+if (opts.i || opts.index) {
+  const index = opts.i || opts.index;
+  if (typeof config.server !== 'object') {
+    config.server = {};
+  }
+  config.server.index = index;
+}
+
+bs.init(config);
