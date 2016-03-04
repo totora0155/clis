6ea34481e88f909907d371f38f10ddbd9a79d73c
nju33 <sski0155@gmail.com>
2016-03-04 14:29:56 +0900

    Implement to save history of bs cmd by json

--- bs/index.js
+++ bs/index.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const jsonfile = require('jsonfile');
 const exec = require('child_process').exec;
 const assign = require('object-assign');
 const bs = require('browser-sync').create();
 const config = require('./bs-config');
 const cwd = process.cwd();
 const opts = require('minimist')(process.argv.slice(2));
+
+const historyPath = __dirname + '/history.json';
 
 if (opts.e || opts.edit) {
   exec(`open ${__dirname + '/bs-config.js'}`, (err) => {
@@ -44,4 +47,58 @@ if (opts.i || opts.index) {
   config.server.index = index;
 }
 
+addHistory();
 bs.init(config);
+
+function addHistory() {
+  ensureExists(historyPath, {histories: []})
+    .then(readJson)
+    .then(writeJson.bind(null, config))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+}
+
+function ensureExists(jsonPath, initValue) {
+  return new Promise((resolve) => {
+    fs.access(jsonPath, fs.F_OK, (err) => {
+      if (err) {
+        fs.writeFile(jsonPath, JSON.stringify(initValue), 'utf-8', () => {
+          return resolve({jsonPath});
+        });
+      } else {
+        return resolve({jsonPath});
+      }
+    });
+  })
+}
+
+function readJson(data) {
+  const jsonPath = data.jsonPath;
+
+  return new Promise((resolve, reject) => {
+    jsonfile.readFile(jsonPath, (err, obj) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve({jsonPath, obj});
+    });
+  });
+}
+
+function writeJson(config, data) {
+  const jsonPath = data.jsonPath;
+  const obj = data.obj;
+  const history = `bs ${process.argv.slice(2).join(' ')}`.trim();
+  obj.histories.unshift(history);
+
+  return new Promise((resolve, reject) => {
+    jsonfile.writeFile(jsonPath, obj, (err) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve();
+    })
+  });
+}
